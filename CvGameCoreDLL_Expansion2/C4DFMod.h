e333e60304e765cb0d67e952c9a6f93d041f88c4
Michael Whiter <mjwhiter@hotmail.com>
2016-03-06 11:15:02 -0500

    Set up to align with CPP, with a custom GUID

---
+++ CvGameCoreDLL_Expansion2/C4DFMod.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef C4DF_MOD_H
+#define C4DF_MOD_H
+
+// Defining this as true, because this is my mod! (for CPP, this define is defined more complex. Unsuitable for my individual purposes)
+#define MOD_DIPLOMACY_CIV4_FEATURES true
+
+#define MOD_DLL_C4DF_GUID { 0xcd9958cc, 0xfa3f, 0x468e, { 0x89, 0xdc, 0x34, 0x6e, 0x9d, 0x30, 0x36, 0xaf } };
+#define MOD_DLL_C4DF_NAME "Civilization IV Diplomatic Features v1"
+#define MOD_DLL_C4DF_VERSION_NUMBER ((uint) 1)
+
+#endif
